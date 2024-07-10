72da907af29a53db0da1ff58ad15d896b02be6e8
Saxon Thune <43625690+saxonthune@users.noreply.github.com>
2024-07-10 11:24:41 -0400

    footnote, aside, code style

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,21 @@
+const markdownIt = require("markdown-it");
 const pluginImages = require("./eleventy.config.images.js");
+const pluginShortcodes = require("./eleventy.config.shortCodes.js");
 
 module.exports = function(eleventyConfig) {
+
+    const md = markdownIt({
+        html: true,
+        linkify: true
+    }).use(require("markdown-it-footnote"));
+
+    eleventyConfig.setLibrary("md", md);
+
+
     eleventyConfig.addPassthroughCopy("./public");
 
     eleventyConfig.addWatchTarget("public/**/*.{svg,webp,png,jpeg}");
 
     eleventyConfig.addPlugin(pluginImages);
+    eleventyConfig.addPlugin(pluginShortcodes);
 };
--- eleventy.config.shortCodes.js
+++ eleventy.config.shortCodes.js
@@ -1,4 +1,13 @@
-module.exports = function(eleventyConfig) {
-    
+const markdownIt = require("markdown-it");
 
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPairedAsyncShortcode(
+        "aside-md", 
+        async function(content, cssClass) {
+            const md = markdownIt();
+            const renderedContent = await md.render(content);
+            return `<aside class="${cssClass}">${renderedContent}</aside>`;
+        
+        }
+    );
 };
